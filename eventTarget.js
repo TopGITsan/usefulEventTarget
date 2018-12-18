81fd1fd058400be8eae4c2f4d669fa67f2db138f
iTop <itop@192-168-0-100.rdsnet.ro>
2018-12-18 17:55:34 +0200

    set the same event handler on multiple divs

--- eventTarget.js
+++ eventTarget.js
@@ -2,3 +2,16 @@ for (let i=0; i<16; i++){
     let myDiv = document.createElement('div');
     document.body.appendChild(myDiv);
 }
+
+function random(num){
+    return Math.floor(Math.random()*num);
+}
+
+function bgColor(){
+    let newColor= 'rgb('+random(255)+','+random(255)+','+random(255)+','+0.5+')';
+    return newColor;
+}
+
+let divs = document.querySelectorAll('div');
+
+divs.forEach(div=> div.onclick=(e)=> e.target.style.backgroundColor = bgColor());
